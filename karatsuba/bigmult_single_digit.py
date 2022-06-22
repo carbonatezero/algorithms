aae77f4ab10d6352f57fcdaa35adbf920380cdb1
carbonate <carbonatezero@gmail.com>
2022-06-22 17:24:52 -0400

    fixed bug

--- karatsuba/bigmult_single_digit.py
+++ karatsuba/bigmult_single_digit.py
@@ -6,28 +6,32 @@ def kara(x, y):
         raise ValueError('zero length number found.')
 
     if (l1==1) and (l2==1):
-        return int(x) * int(y) # fall back to single digit multiplication 
+        return str(int(x)*int(y)) # fall back to single digit multiplication 
+
+    # padding 0 to the shorter digit string
+    if l1 < l2:
+        x = (l2-l1)*'0' + x
+    elif l2 < l1:
+        y = (l1-l2)*'0' + y
+
+    # Calculates the half of the longer digit string.
+    l = max(l1,l2)
+    m = l // 2
     
-    # Calculates the size of the larger numbers.
-    m = max(l1, l2) // 2
-    
-    # Split the digit sequences in the middle. 
-    if l1 == 1:
-        x1, x0 = '0', x[:]
-    else:
-        x1, x0 = x[:l1-m], x[l1-m:]
-    
-    if l2 == 1:
-        y1, y0 = '0', y[:]
-    else:
-        y1, y0 = y[:l2-m], y[l2-m:]
+    # Split the digit string in the middle. 
+    x1, x0 = x[:l-m], x[l-m:]
+    y1, y0 = y[:l-m], y[l-m:]
 
     # 3 recursive calls made to numbers approximately half the size.
     z0 = kara(x0, y0)
     z2 = kara(x1, y1)
-    z1 = kara(str(int(x0) + int(x1)), str(int(y0) + int(y1))) - z2 - z0
+    z1 = int(kara(str(int(x0) + int(x1)), str(int(y0) + int(y1)))) - int(z2) - int(z0)
     
-    return ((z2 * 10 ** (m * 2)) + (z1 * 10 ** m) + z0)
+    z2 = z2 + (m*2)*'0'
+    z1 = str(z1) + (m)*'0'
+    
+    return str(int(z2)+int(z1)+int(z0))
+    # return str((z2 * 10 ** (m * 2)) + (z1 * 10 ** m) + z0)
 
 def main():
     a = '3141592653589793238462643383279502884197169399375105820974944592'
@@ -36,5 +40,6 @@ def main():
     b = '36'
     c = kara(a,b)
     print(c)
+    print(int(a)*int(b))
 if __name__ == '__main__':
     main()
